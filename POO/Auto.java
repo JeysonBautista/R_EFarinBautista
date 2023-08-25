938566543eacc6a6f9df08ca9c0328f36db64347
JeysonBautista <111783892+JeysonBautista@users.noreply.github.com>
2023-08-25 12:50:55 -0500

    .

---
+++ POO/Auto.java
@@ -0,0 +1,19 @@
+package POO;
+
+
+public class Auto {
+    
+    String Color;
+    String Marca;
+    double KM;
+    
+    public void encender(){
+        System.out.println("El coche "+ Color + " esta encendido");
+    };
+    public void acelerar(){
+        System.out.println("El auto acelera");
+    };
+    public void frenar(){
+        System.out.println("El auto frena");
+    };
+}
